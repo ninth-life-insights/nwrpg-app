f46c62fa2e20de4bed4b7ac377aa0e044902d1d6
ninth-life-insights <cat@ninthlifeinsights.com>
2025-09-18 21:20:18 -0700

    feat/isMissionOverdue datehelper fn

--- utils/dateHelpers.js
+++ utils/dateHelpers.js
@@ -15,3 +15,32 @@ export const toDateString = (date) => {
   return dayjs(date).format('YYYY-MM-DD');
 };
 
+//return dayJS object from date string
+export const fromDateString = (dateString) => {
+  return dayjs(dateString, 'YYYY-MM-DD');
+};
+
+export const isMissionOverdue = (mission) => {
+  if (!mission.dueDate || mission.status === MISSION_STATUS.COMPLETED) {
+    return false;
+  };
+
+  const today = dayjs();
+  const dueDate = dayjs(mission.dueDate);
+
+  return dayjs(mission.dueDate).isBefore(dayjs(), 'day');
+};
+
+
+// Day-only comparison functions using dayjs unit parameter
+export const isSameDate = (date1, date2) => {
+  return dayjs(date1).isSame(dayjs(date2), 'day');
+};
+
+export const isBeforeDate = (date1, date2) => {
+  return dayjs(date1).isBefore(dayjs(date2), 'day');
+};
+
+export const isAfterDate = (date1, date2) => {
+  return dayjs(date1).isAfter(dayjs(date2), 'day');
+};
